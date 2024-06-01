7ff735bb867adecd37f965164dfc64f3adf615fd
Mitchell Dederer <mitchelldederer@gmail.com>
2024-06-02 00:58:05 +1000

    test: testing something

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store.ts'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
