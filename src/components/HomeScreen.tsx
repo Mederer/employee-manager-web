5e2533f80c1f69505ffc1c01a8235ac1e798f8cc
Mitchell Dederer <mitchelldederer@gmail.com>
2024-06-03 16:41:19 +1000

    feat: test

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -5,6 +5,7 @@ function HomeScreen() {
         <div className="mx-auto hover:scale-105 transition-transform">
             <HeroTitle title="Employee Manager" subtitle="Manage your employees with ease" />
             <div className="text-center mt-4">"Recommended by experts"</div>
+            <div>Hi Seb</div>
         </div>
     );
 }
